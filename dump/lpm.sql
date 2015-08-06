12843dca7ec09c5f56dfc4dbadd0ce83f9664b7e
Thibaud05 <t.granier@studio05.fr>
2015-08-06 08:48:49 +0200

    database dump

--- dump/lpm.sql
+++ dump/lpm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 31 Juillet 2015 à 09:43
+-- Généré le :  Jeu 06 Août 2015 à 08:47
 -- Version du serveur :  5.6.15-log
 -- Version de PHP :  5.5.8
 
@@ -78,9 +78,9 @@ CREATE TABLE IF NOT EXISTS `release` (
 --
 
 INSERT INTO `release` (`id`, `id_type`, `name`, `day`) VALUES
-(1, 1, 'QA', '2015-08-05'),
-(2, 2, 'QA', '2015-08-10'),
-(3, 1, 'PRD', '2015-08-07');
+(1, 1, 'QA', '2015-08-12'),
+(2, 2, 'QA', '2015-08-18'),
+(3, 1, 'PRD', '2015-08-14');
 
 -- --------------------------------------------------------
 
@@ -135,29 +135,60 @@ CREATE TABLE IF NOT EXISTS `task` (
   `date_creation` date NOT NULL,
   `date_finish` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=23 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=33 ;
 
 --
 -- Contenu de la table `task`
 --
 
 INSERT INTO `task` (`id`, `name`, `id_user_add`, `id_user_responsible`, `id_user_accountable`, `id_status`, `id_type`, `id_cat`, `id_subcat`, `priority`, `version`, `comments`, `comments_valid`, `date_creation`, `date_finish`) VALUES
-(2, '', 0, 1, 0, 0, 4, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-10'),
-(3, 'test01', 0, 0, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
-(5, 'test03', 0, 2, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-07-29'),
-(6, 'test04', 0, 0, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
-(8, 'test07', 0, 0, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
-(9, 'test08', 0, 2, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-07-27'),
-(10, 'test09', 0, 3, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-07-27'),
-(12, 'test11', 0, 1, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-07-29'),
-(13, 'Bug IE retour gauche', 0, 2, 0, 0, 1, 0, 0, 0, '', 'Sur les ecrant ', '', '0000-00-00', '2015-08-04'),
-(14, 'test13', 0, 3, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-07-28'),
-(15, 'test14', 0, 2, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-07-28'),
-(16, 'test15', 0, 0, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
-(18, 'test18', 0, 0, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
-(22, 'Waterfall', 0, 5, 0, 0, 1, 0, 0, 0, '', 'Vérifier que le prix est calculé sur les trois derniers mois.<br><br>Pour le waterfall factory le prix utilisé doit être celui de la region de la factory.', '', '0000-00-00', '2015-07-31'),
+(2, '', 0, 4, 0, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-03'),
+(3, '', 0, 4, 0, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-04'),
+(5, 'test03', 0, 2, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-12'),
+(6, '', 0, 4, 0, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-05'),
+(8, '', 0, 4, 0, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-06'),
+(9, 'test08', 0, 3, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
+(10, 'test09', 0, 2, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
+(12, '', 0, 2, 0, 0, 1, 0, 0, 0, '', '', '', '0000-00-00', '2015-08-11'),
+(13, 'Bug IE retour gauche', 0, 2, 0, 0, 1, 0, 0, 0, '', 'Sur les ecrans PDF market et factory', '', '0000-00-00', '2015-08-04'),
+(14, 'test13', 0, 1, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '0000-00-00'),
+(15, 'test14', 0, 2, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-14'),
+(16, '', 0, 4, 0, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-07'),
+(18, '', 0, 3, 0, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-03'),
+(22, 'Waterfall', 0, 2, 0, 0, 1, 0, 0, 0, '', 'Vérifier que le prix est calculé sur les trois derniers mois.<br><br>Pour le waterfall factory le prix utilisé doit être celui de la region de la factory.', '', '0000-00-00', '2015-08-05'),
 (20, 'Bug première ligne', 0, 2, 0, 0, 1, 0, 0, 0, '', 'Sur project details, quand un cellule est modifiée la grille est recharger et le focus est perdu.', '', '0000-00-00', '2015-08-03'),
-(21, 'toto', 0, 1, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-07-27');
+(21, 'toto', 0, 2, 0, 0, 1, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-13'),
+(24, '', 1, 3, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-04'),
+(23, '', 1, 3, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-05'),
+(25, '', 1, 3, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-06'),
+(26, '', 1, 3, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-07'),
+(27, '', 1, 5, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-03'),
+(28, '', 1, 5, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-04'),
+(29, '', 1, 5, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-05'),
+(30, '', 1, 5, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-06'),
+(31, '', 1, 5, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-07'),
+(32, '', 1, 1, 1, 0, 5, 0, 0, 0, '', 'text', '', '0000-00-00', '2015-08-10');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `task_file`
+--
+
+CREATE TABLE IF NOT EXISTS `task_file` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_task` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(90) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `task_file`
+--
+
+INSERT INTO `task_file` (`id`, `id_task`, `title`, `type`) VALUES
+(4, 5, 'testFile.jpg', 'image/jpeg');
 
 -- --------------------------------------------------------
 
@@ -205,7 +236,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 INSERT INTO `user` (`id`, `id_group`, `email`, `password`, `firstname`, `lastname`) VALUES
 (1, 1, 'admin@lpm.com', '0000', 'Thibaud', 'Granier'),
-(2, 0, '', '', 'fleury', ''),
+(2, 0, 'fleury@segeco.fr', '0000', 'fleury', ''),
 (3, 0, '', '', 'kevin', ''),
 (4, 0, '', '', 'patrice', ''),
 (5, 0, '', '', 'nayed', '');
