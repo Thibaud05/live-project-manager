d72145ffccafd031583d49106c8d784a3d321397
Thibaud05 <t.granier@studio05.fr>
2016-01-15 13:42:05 +0100

    remove comments

--- server.js
+++ server.js
@@ -23,17 +23,6 @@ appExpress.get('/', function (req, res) {
 });
 
 connection.connect();
-
- /*
-
-$json->addSql("tasks","SELECT id,id_user_responsible as id_user,name as title,id_type,date_finish as day, comments as description ,
- id_user_add as creationUser, date_creation as creationDate, priority, valid,
- id_user_accountable as accountableUser, date_update as updateDate FROM task ORDER BY priority");
-$json->addSql("taskTypes","SELECT * FROM type");
-$json->addSql("releases","SELECT * FROM `release`");
-$json->addSql("users","SELECT id,firstname, lastname,id_group as level FROM user");
-$json->addSql("tasks_files","SELECT * FROM task_file");
-*/
 var sqls = [
   "SELECT * FROM `type`",
   "SELECT * FROM `release`",
