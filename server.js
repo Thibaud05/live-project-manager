8c48e9fccaef703139964b3997d3df3982738020
Thibaud05 <t.granier@studio05.fr>
2017-01-25 07:45:36 +0100

    fix
    selectedProject

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ io.on('connection', function (socket) {
     //console.log(u)
     var html = app.display(u)
     global.data.connectUserId = u.id
-    global.data.connectUserId = u.selectedProject
+    global.data.selectedProject = u.selectedProject
     socket.connectUserId = u.id
     var obj = {logged:u.logged,key:u.getKey(),html:html}
     socket.emit('logged',{obj:obj,data:global.data});
